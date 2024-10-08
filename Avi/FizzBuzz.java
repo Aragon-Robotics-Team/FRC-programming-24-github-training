15418eb301af3692d40f0f17a75fed38e01d78b3
aviilol <100895020+aviilol@users.noreply.github.com>
2024-10-08 16:13:12 -0700

    Update FizzBuzz.java
    fixed fibonacci code

--- Avi/FizzBuzz.java
+++ Avi/FizzBuzz.java
@@ -1,14 +1,16 @@
 public class Main {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         int index = 11;
         int[] fibonacci = new int[index];
-		fibonacci[1] = 1;
-        fibonacci[0] = 1;
-		for (int n = 2; n < 11; n++) {
-			fibonacci[n] = fibonacci[n-1] + fibonacci[n-2];
-		}
-		for (int x = 0; x < 11; x++) {
-			System.out.println(fibonacci[x]);
-		}
-	}
+        fibonacci[0] = 1; 
+        fibonacci[1] = 1;
+
+        for (int n = 2; n < index; n++) {
+            fibonacci[n] = fibonacci[n - 1] + fibonacci[n - 2];
+        }
+
+        for (int x = 0; x < index; x++) {
+            System.out.println(fibonacci[x]);
+        }
+    }
 }
